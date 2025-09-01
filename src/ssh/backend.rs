cb7efa87786ce9e239e97d3133e04b9ad140451c
veeso <christian.visintin@veeso.dev>
2025-09-01 16:39:36 +0200

    style: lint

--- src/ssh/backend.rs
+++ src/ssh/backend.rs
@@ -91,6 +91,7 @@ pub trait Sftp {
         T: AsRef<Path>;
 
     /// Resolve the real path for `path`.
+    #[allow(dead_code)]
     fn realpath(&self, path: &Path) -> RemoteResult<PathBuf>;
 
     /// Renames a file from `src` to `dest`.
